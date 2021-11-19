4979399cdca85568e4dfbbda04bae4de7b1c9141
wachira_Dev <ewachira254@gmail.com>
2021-11-20 02:18:32 +0300

    @tdn/ui[chore]: remove redux setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,23 +3,17 @@ import "prismjs/themes/prism-tomorrow.css";
 import "tailwindcss/tailwind.css";
 import { setup } from "goober";
 import { prefix } from "goober-autoprefixer";
-import { Provider } from "react-redux";
 import AppLayout from "../layouts/app-layout";
 
 import "../styles.css";
 import React from "react";
-import store from "../redux/store";
 
 setup(React.createElement, prefix);
 
 export default function MyApp({ Component, pageProps }: any) {
   const getLayout =
     Component?.getLayout ||
-    ((page: React.ReactNode) => (
-      <Provider store={store}>
-        <AppLayout children={page} />
-      </Provider>
-    ));
+    ((page: React.ReactNode) => <AppLayout children={page} />);
 
   return getLayout(<Component {...pageProps} />);
 }
