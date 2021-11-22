8ca9c3559511344bdf0058559d268f5d85e4c341
wachira_Dev <ewachira254@gmail.com>
2021-11-22 03:25:30 +0300

    @tdn/ui[feature]: add splitbee analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,11 +6,19 @@ import { prefix } from "goober-autoprefixer";
 import AppLayout from "../layouts/app-layout";
 
 import "../styles.css";
-import React from "react";
+import React, { useEffect } from "react";
+import splitbee from "@splitbee/web";
 
 setup(React.createElement, prefix);
 
 export default function MyApp({ Component, pageProps }: any) {
+  useEffect(() => {
+    splitbee.init({
+      scriptUrl: "/bee.js",
+      apiUrl: "/_hive",
+    });
+  }, []);
+
   const getLayout =
     Component?.getLayout ||
     ((page: React.ReactNode) => <AppLayout children={page} />);
