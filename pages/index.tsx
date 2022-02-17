c34c54d6e19cb3359dd802552524d3655b32213f
wachira_Dev <31078302+tesh254@users.noreply.github.com>
2022-02-17 12:43:56 +0300

    @tdn/frontend[bug]: don't show draft articles

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ function urlFor(source: any) {
 
 export const getServerSideProps = async () => {
   const response = await sanityClient.fetch(`
-    *[_type == "post" && publishedAt < now()]|order(publishedAt desc){
+    *[_type == "post" && publishedAt < now() && !(_id in path('drafts.**'))]|order(publishedAt desc){
       title,
       body,
       slug,
