975af67ddfd0b42ed3bab63f4b005b2551df55d4
tesh254 <ewachira254@gmail.com>
2023-10-17 00:16:20 +0300

    add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import "react-notion/src/styles.css";
 import "tailwindcss/tailwind.css";
-import splitbee from "@splitbee/web";
 import { Analytics } from "@vercel/analytics/react";
 import { setup } from "goober";
 import { prefix } from "goober-autoprefixer";
@@ -8,18 +7,11 @@ import AppLayout from "../layouts/app-layout";
 
 import "../styles.css";
 import "../public/prism.css";
-import React, { useEffect } from "react";
+import React from "react";
 
 setup(React.createElement, prefix);
 
 export default function MyApp({ Component, pageProps }: any) {
-  useEffect(() => {
-    splitbee.init({
-      apiUrl: "/sb-api",
-      scriptUrl: "/sb.js",
-    });
-  }, []);
-
   const getLayout =
     Component?.getLayout ||
     ((page: React.ReactNode) => <AppLayout>{page}</AppLayout>);
